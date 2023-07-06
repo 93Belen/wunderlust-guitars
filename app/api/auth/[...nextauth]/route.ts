7d184015fd6ca21827af97bc3948979340a9e479
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-06 11:29:23 -0500

    google and spotify login working

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import EmailProvider from "next-auth/providers/email"
 import SpotifyProvider from "next-auth/providers/spotify"
+import CognitoProvider from "next-auth/providers/cognito"
+import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -10,18 +12,23 @@ const prisma = new PrismaClient()
 export const authOptions = {
 	adapter: PrismaAdapter(prisma),
   providers: [
-      GoogleProvider({
-          clientId: process.env.GOOGLE_ID as string,
-          clientSecret: process.env.GOOGLE_SECRET as string
-     }),
      EmailProvider({
         server: process.env.EMAIL_SERVER,
         from: process.env.EMAIL_FROM
       }),
+      CognitoProvider({
+        clientId: process.env.COGNITO_CLIENT_ID as string,
+        clientSecret: process.env.COGNITO_CLIENT_SECRET as string,
+        issuer: process.env.COGNITO_ISSUER,
+      }),    
       SpotifyProvider({
         clientId: process.env.SPOTIFY_CLIENT_ID as string,
         clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
-      })
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string
+   }),
   ]
 }
 const handler = NextAuth(authOptions)
