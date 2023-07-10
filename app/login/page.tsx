c53fd06c43f68a1eccb23bce91b3d21f2abddf39
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-10 06:40:08 -0500

    added dynamic email input to login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { Magic, RPCError, RPCErrorCode } from 'magic-sdk';
+import { useState } from 'react';
 
 export default function Login(){
+    const [email, setEmail] = useState("")
 
     const onSubmit = async() => {
         try{
             const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY as string)
-            const didToken = await magic.auth.loginWithMagicLink({email: 'belengm@live.com'})
+            const didToken = await magic.auth.loginWithMagicLink({email: email})
             console.log(didToken)
         }
         catch(err){
@@ -24,9 +26,14 @@ export default function Login(){
         
     }
 
+    const onChange = (e) => {
+        const email = e.target.value;
+        setEmail(email)
+    }
+
     return (
         <div className="p-10">
-            <input className='bg-slate-300 m-5' type="text" name="" id="" />
+            <input onChange={onChange} className='bg-slate-300 m-5' type="email" name="" id="" />
             <button onClick={onSubmit} type='submit' className='bg-lime-500 p-2 rounded-md'>Log in</button>
         </div>
     )
