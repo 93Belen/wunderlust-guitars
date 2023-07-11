d972631a3e77d7d5c72232540b675d6c2ec89cac
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-11 11:13:37 -0500

    calling api endpoints first try

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,22 @@ export default function Login(){
             const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY as string)
             const didToken = await magic.auth.loginWithMagicLink({email: email})
             const data = await magic.user.getMetadata()
-            console.log(data)
+            if(didToken){
+                try{
+                    const res = await fetch('api/user/updateuserdate', {
+                        method: 'POST',
+                        body: JSON.stringify({email})
+                      })
+                      return res.json()
+                }
+                catch(e){
+                    const res = await fetch('api/user/addnewuser', {
+                        method: 'POST',
+                        body: JSON.stringify({email})
+                      })
+                      return res.json()
+                }
+            }
         }
         catch(err){
             if (err instanceof RPCError) {
