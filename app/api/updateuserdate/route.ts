e8f59d73451c353426ee6ecadb0f1682a8842ee2
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-11 11:08:07 -0500

    added api routes

---
+++ app/api/updateuserdate/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "components/prisma/seed";
+
+
+export async function POST(req: Request) {
+
+    const { email } = await req.json();
+    
+    
+    const responseFromPrisma = await prisma.user.update({
+        where: {
+            email: email
+        },
+        data: {
+            last_logged: new Date()
+        }
+    })
+  
+    return new Response(JSON.stringify(responseFromPrisma));
+  }
+  
