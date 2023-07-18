bfa2a2dc2045d8a22703718977b1c6f271ca429a
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-18 10:12:03 -0500

    starting from 0

--- app/api/favorites/getmyfavorites/route.ts
+++ app/api/favorites/getmyfavorites/route.ts
@@ -10,21 +10,6 @@ export async function POST(req: Request): Promise<Response>  {
             email: email as string
         }
     })
-    console.log(responseFromPrisma)
-
-
-     const createGuitar = await prisma.guitar.create()
-    // const createPostAndCategory = await prisma.user.create({
-    //     data: {
-    //       id: 'clk8esfff000134hzm2pkcjmf',
-    //       favorites: {
-    //         create: [{ id: '1oleole' }],
-    //       },
-    //     },
-    //   })
-
-
-
   
     return new Response(JSON.stringify(responseFromPrisma));
   }
