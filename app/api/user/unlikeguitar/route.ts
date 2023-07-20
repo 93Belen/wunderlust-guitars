d9e6e6aae57f9f6c94c4895b97bd4895fa00ea4d
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-20 13:25:12 -0500

    unline route works

--- app/api/user/unlikeguitar/route.ts
+++ app/api/user/unlikeguitar/route.ts
@@ -5,13 +5,11 @@ export async function POST(req: Request): Promise<Response> {
 
     const { guitarId, userId }: {guitarId: string, userId: string} = await req.json();
       
-        const responseFromPrisma = await prisma.userFavorites.delete({
+        const responseFromPrisma = await prisma.userFavorites.deleteMany({
           where: {
-            userandguitar:{
-                userId: userId, // Provide the userId value here
+                userId: userId,
                 guitarId: guitarId,
-            }
-          },
+          }
         });
       
         return new Response(JSON.stringify(responseFromPrisma));
