6950d0e515e0e6ead1bba0048ce15eae45a5892e
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-24 18:10:39 -0500

    added with to checkout button

--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -2,7 +2,7 @@
 
 export default function Checkout() {
     return (
-        <button className='py-[0.5rem] px-[1rem] bg-purple font-mono text-white rounded-lg text-[1.125rem] font-normal'>
+        <button className='py-[0.5rem] px-[1rem] bg-purple font-mono text-white rounded-lg text-[1.125rem] font-normal w-[8rem]'>
             Checkout
         </button>
     )
