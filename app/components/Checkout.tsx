d5a63630696accf8aa2238ec235d35beb79db959
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-24 07:07:19 -0500

    checkout

---
+++ app/components/Checkout.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Checkout() {
+    return (
+        <button className='py-[0.5rem] px-[1rem] bg-purple font-mono text-white rounded-lg text-[1.125rem] font-normal'>
+            Checkout
+        </button>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import AddToFavorites from "./components/AddFavorite";
 import AddToCart from "./components/AddToCart";
 import Card from "./components/Card";
+import Checkout from "./components/Checkout";
 
 export default function Home() {
   return (
     <main className='bg-black min-h-screen'>
      <AddToFavorites /><br />
      <AddToCart /><br />
-     <Card />
+     <Card /><br />
+     <Checkout />
     </main>
   )
 }
