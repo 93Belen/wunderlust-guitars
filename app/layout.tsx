2303845ce6bb77c4fc5c1c5580f44b82e60b64a6
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-24 05:24:32 -0500

    added component files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from './components/header'
+import Header from './components/Header'
 import Hydrate from './components/Hydrate'
 import './globals.css'
 import { Goldman, Play } from "@next/font/google"
