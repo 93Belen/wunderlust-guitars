91c8646b812a205e924474a21ab4cbff29a19dee
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-24 05:35:36 -0500

    added addtofavorites

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
+import AddToFavorites from "./components/AddFavorite";
 
 export default function Home() {
   return (
-    <main >
-     
+    <main className='bg-black min-h-screen'>
+     <AddToFavorites />
     </main>
   )
 }
