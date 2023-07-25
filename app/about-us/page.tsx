7b9cbe97622a0402fdd916a060aaff18713f4bae
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-25 17:53:17 -0500

    about us page in progress

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,22 @@
 export default function About(){
     return (
         <main className='h-fit min-h-screen px-4 md:px-8 w-screen overflow-x-hidden'>
-
+            <section>
+                <h1 className='text-white font-sans text-[2.25rem] md:text-[4.5rem] font-[700]'>We are <br />
+                Wunderlust Guitars</h1>
+            </section>
+            <section>
+                
+            </section>
+            <section>
+                
+            </section>
+            <section>
+                
+            </section>
+            <section>
+                
+            </section>
         </main>
     )
 }
