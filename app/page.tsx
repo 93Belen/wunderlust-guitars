2072f53cf0115cd22ef53f586c7a1bf6ae720960
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-25 12:10:30 -0500

    home page about section in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
+import Image from "next/image";
 import Link from "next/link";
-import AddToFavorites from "./components/AddFavorite";
-import AddToCart from "./components/AddToCart";
 import Card from "./components/Card";
 import SoundWave from "./components/SoundWave";
 
@@ -13,11 +12,11 @@ export default function Home() {
       <section className='flex flex-col items-end pr-0 md:pr-2 w-[90vw] mt-[-5rem] lg:mt-[-7rem] lg:pr-24'>
         <div>
           <h1 className='text-white font-sans text-[3rem] font-[700] md:text-[5.375rem] leading-[2.65625rem] md:leading-[4.1875rem] tracking-[-0.20rem] md:tracking-[-0.10rem]'>Wunderlust<br/>Guitars</h1>
-          <h2 className='text-red font-serif tex-[1.rem] md:text-[2.3125rem]'>Handpicked, globally sourced</h2>
+          <h2 className='text-red font-serif text-[1.rem] md:text-[2.3125rem]'>Handpicked, globally sourced</h2>
         </div>
       </section>
-      <section className='pt-16 flex gap-1 flex-col'>
-        <div>
+      <section className='pt-16 md:pt-60 flex gap-1 flex-col'>
+        <div className='md:ml-[-3rem]'>
           <h3 className='font-sans text-[4.5rem] font-[700] leading-[3.1875rem] md:leading-[6rem] text-gray tracking-[-0.20rem] md:tracking-[-0.11rem] md:text-[10rem]'>Most Popular</h3>
           <h4 className='text-white font-mono text-[1.7rem] font-[700] mt-[-1.8rem] pl-5 leading-[3.35625rem] md:leading-[-5rem] md:ml-[7.5%] tracking-[-0.10rem] md:tracking-[-0.05rem] md:text-[3rem]'>Search our most viewed</h4>
         </div>
@@ -35,6 +34,31 @@ export default function Home() {
           <div className='hidden md:block'>
             <Card />
           </div>
+        </div>
+      </section>
+      <section className='pt-28 mb-28 md:mb-80 flex flex-col md:grid md:grid-cols-[30%_70%] md:grid-rows-[40%_60%] md:pt-60 h-fit'>
+        <div className='col-start-2 flex flex-col items-end pr-10 md:mr-[-5rem] h-fit'>
+            <div>
+              <h3 className='font-sans text-[4.5rem] font-[700] leading-[3.1875rem] md:leading-[6rem] text-gray tracking-[-0.20rem] md:tracking-[-0.11rem] md:text-[10rem]'>About Us</h3>
+              <h4 className='text-white font-mono text-[1.7rem] font-[700] mt-[-1rem] pl-2 leading-[1.35625rem] md:leading-[-5rem] tracking-[-0.10rem] md:tracking-[-0.05rem] md:text-[3rem]'>The story behind Wunderlust</h4>
+            </div>
+          </div>
+          <div className='w-[100%] h-auto row-span-2 row-start-1 flex justify-center mt-10'>
+            <Image
+          src='/homepageabout.png'
+          width={2500}
+          height={2500}
+          alt='about'
+          className='w-[90%]'
+          />
+          </div>
+        <div className='h-fit flex justify-end md:px-[10%] mx-[2%] flex-col'>
+          <p className='text-white font-mono text-[1.5rem] md:p-2 py-4 md:max-w-[40rem]'>
+          This OCD-type-of-guy has taken his guitar addiction to a whole new level.
+          But make no mistake, he ain't no random fool who fell off a Kansas farm truck.
+          No, sir!  This guy toured through North America, Europe, and the "God-save-the-Queen" UK.
+          </p>
+          <Link href='' className='text-pink font-mono text-[1.125rem] md:text-[1.5rem] ml-1'>Read more</Link>
         </div>
       </section>
     </main>
