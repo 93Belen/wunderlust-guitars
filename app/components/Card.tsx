f048467b009339eb337f5698e1333fab4f941583
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-26 15:18:37 -0500

    fixing image in card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -7,17 +7,17 @@ import Image from "next/image";
 export default function Card() {
   return (
     <div className={`bg-darkgray flex flex-col p-[1.25rem] justify-center items-center rounded-lg w-[100%] h-fit box-border gap-[1rem] max-w-[300px]`}>
-      <div className="w-[100%] h-auto bg-gray rounded-lg">
+      <div className="w-[100%] bg-gray rounded-lg bg-[url('/placeholderpic.jpg')] h-[17rem] md:h-[12rem] lg:h-[17rem] bg-center bg-no-repeat bg-contain">
           <div className='w-full flex justify-end pr-2 pt-2 top-1'>
             <AddToFavorites />
           </div>
-        <Image
+        {/* <Image
           src="/placeholderpic.jpg"
           height={500}
           width={500}
           alt="guitar"
           className="rounded-lg h-[100%] w-[100%] md:w-[14.75rem]"
-        />
+        /> */}
       </div>
       <div className='w-full h-fit text-white font-mono font-normal'>
         <p>
