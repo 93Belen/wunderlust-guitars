545e9253c368023f21f03b860ed3e61e45a6f7d9
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-27 16:49:27 -0500

    fixed wrong link

--- app/shop/bass/page.tsx
+++ app/shop/bass/page.tsx
@@ -1,7 +1,20 @@
+import Card from "components/app/components/Card"
+
 export default function Bass(){
     return (
-        <section>
-            <h1>Bass</h1>
-        </section>
+        <section className='w-full max-w-[550px] md:max-w-none grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:w-[100%] lg:max-w-[1250px]'>
+        <div className='h-[15rem] lg:h-[22rem]'>
+            <Card />
+        </div>
+        <div className='h-[15rem] lg:h-[22rem]'>
+            <Card />
+        </div>
+        <div className='h-[15rem] lg:h-[22rem]'>
+            <Card />
+        </div>
+        <div className='h-[15rem]  lg:h-[22rem]'>
+            <Card />
+        </div>
+    </section>
     )
 }
--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -11,7 +11,7 @@ export default function Shop(
         <main className='h-fit min-h-screen px-4 md:px-8 w-screen overflow-x-hidden box-border pt-10 md:pt-20'>
             <div className='w-full flex justify-center'>
             <header className='flex justify-around w-full md:max-w-[800px] text-white h-fit font-mono text-[1.125rem]'>
-                <Link href="/shop/mostpopular">Most Popular</Link>
+                <Link href="/shop">Most Popular</Link>
                 <Link href="/shop/acoustic">Acoustic</Link>
                 <Link href="/shop/electric">Electric</Link>
                 <Link href="/shop/bass">Bass</Link>
