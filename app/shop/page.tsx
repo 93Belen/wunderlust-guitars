bac555e89657a32997ca1d7e95d98d038f01e3a5
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-30 11:28:35 -0500

    getting products from stripe

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,32 +1,21 @@
+"use client"
+
+import { useWebStore } from "components/store";
 import Card from "../components/Card";
 
 export default function Popular(){
+    const store = useWebStore()
+    const allGuitars = store.allGuitars
+    console.log(allGuitars)
+
+
     return (
         <section className='w-full max-w-[550px] md:max-w-none grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:w-[100%] lg:max-w-[1250px]'>
-            <div className='h-[15rem] lg:h-[22rem]'>
+            {allGuitars.map((guitar) => (
+                <div className='h-[15rem] lg:h-[22rem]'>
                 <Card />
             </div>
-            <div className='h-[15rem] lg:h-[22rem]'>
-                <Card />
-            </div>
-            <div className='h-[15rem] lg:h-[22rem]'>
-                <Card />
-            </div>
-            <div className='h-[15rem]  lg:h-[22rem]'>
-                <Card />
-            </div>
-            <div className='h-[15rem] lg:h-[22rem]'>
-                <Card />
-            </div>
-            <div className='h-[15rem]  lg:h-[22rem]'>
-                <Card />
-            </div>
-            <div className='h-[15rem] lg:h-[22rem]'>
-                <Card />
-            </div>
-            <div className='h-[15rem] lg:h-[22rem]'>
-                <Card />
-            </div>
+            ))}
         </section>
     )
 }
