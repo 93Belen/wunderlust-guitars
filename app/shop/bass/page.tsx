fedda14ee92698665b1b4301425666b5e7470cc7
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-07-31 14:48:40 -0500

    filters working

--- app/shop/bass/page.tsx
+++ app/shop/bass/page.tsx
@@ -7,10 +7,15 @@ import Card from "../../components/Card";
 export default function Bass(){
     const store = useWebStore()
     const allGuitars = store.allGuitars
+    const bass = allGuitars.filter(guitar => {
+        if(guitar.metadata.bass === 'true'){
+            return guitar
+        }
+    })
 
     return (
         <section className='w-full max-w-[550px] md:max-w-none grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:w-[100%] lg:max-w-[1250px]'>
-            {allGuitars.map((guitar) => (
+            {bass.map((guitar) => (
                 <div className='h-[15rem] lg:h-[22rem]'>
                <Card data={guitar} />
             </div>
--- app/shop/electric/page.tsx
+++ app/shop/electric/page.tsx
@@ -6,9 +6,14 @@ import Card from "../../components/Card";
 export default function Electric(){
     const store = useWebStore()
     const allGuitars = store.allGuitars
+    const electric = allGuitars.filter(guitar => {
+        if(guitar.metadata.electric === 'true'){
+            return guitar
+        }
+    })
     return (
         <section className='w-full max-w-[550px] md:max-w-none grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:w-[100%] lg:max-w-[1250px]'>
-            {allGuitars.map((guitar) => (
+            {electric.map((guitar) => (
                 <div className='h-[15rem] lg:h-[22rem]'>
                 <Card data={guitar} />
             </div>
