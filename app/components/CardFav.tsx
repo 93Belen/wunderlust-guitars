0744e0c6f2757660b2c27f020e862daa359cba98
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-08-01 08:18:27 -0500

    card fav needs morei nfo from, need to update stripe product

--- app/components/CardFav.tsx
+++ app/components/CardFav.tsx
@@ -2,6 +2,7 @@
 import { initializeMagic } from "components/magic/initializeMagic";
 import { Magic } from "magic-sdk";
 import Image from "next/image"
+import { getModeForResolutionAtIndex } from "typescript";
 import AddToCart from "./AddToCart"
 
 export default function CardFav({guitar}){
@@ -43,14 +44,14 @@ export default function CardFav({guitar}){
         <div className='flex justify-between w-[99%] max-w-[900px]'>
             <div className='flex gap-x-4 h-[8.375rem] rounded-lg'>
                 <Image
-                src="/placeholderpic.jpg"
+                src={guitar.images[0]}
                 height={500}
                 width={500}
                 alt="guitar"
                 className='md:max-w-[8.375rem] max-w-[8.375rem] h-[100%] rounded-lg'
                 />
                 <div className='flex flex-col text-white font-mono md:w-[19.9375rem] justify-between'>
-                    <h2 className='text-[1rem]'>Guitar Name</h2>
+                    <h2 className='text-[1rem]'>{guitar.name}</h2>
                     <p className='text-[0.75rem]'>Color Shell Pink</p>
                     <p className='text-[0.75rem]'>Weight: 12lb</p>
                     <p className='text-[0.75rem]'>$5,000</p>
