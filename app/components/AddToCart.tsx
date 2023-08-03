d8bc2f29f37a1501033006db45147deaa376191a
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-08-03 12:04:00 -0500

    doesnt add same item twice to cart

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -1,21 +1,27 @@
-"use client"
+import { useWebStore } from "components/store";
+import { Product } from "components/types/storeTypes";
 
-import { useWebStore } from "components/store"
-import { Product } from "components/types/storeTypes"
+export default function AddToCart({ guitar }: { guitar: Product }) {
+  const store = useWebStore();
 
-export default function AddToCart({guitar}: {guitar: Product}) {
-    const store = useWebStore()
-    const addGuitarToCart = () => {
-        if(!store.cart.includes(guitar)){
-            store.addToCart(guitar)
-        }
-        else {
-            window.alert("This guitar is already in your cart")
-        }
+  const addGuitarToCart = () => {
+    const existingGuitar = store.cart.find(
+      (item) => item.id === guitar.id
+    );
+
+    if (!existingGuitar) {
+      store.addToCart(guitar);
+    } else {
+      window.alert("This guitar is already in your cart");
     }
-    return (
-        <button onClick={addGuitarToCart} className='py-[0.5rem] px-[1rem] bg-purple font-mono text-white rounded-lg text-[1.125rem] font-normal w-[8rem]'>
-            Add to Cart
-        </button>
-    )
+  };
+
+  return (
+    <button
+      onClick={addGuitarToCart}
+      className='py-[0.5rem] px-[1rem] bg-purple font-mono text-white rounded-lg text-[1.125rem] font-normal w-[8rem]'
+    >
+      Add to Cart
+    </button>
+  );
 }
