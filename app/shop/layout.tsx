f276d491d85b0c8167d748b8d2224440e73ef3bd
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-08-13 13:39:57 -0500

    trying to fix window error

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -12,14 +12,13 @@ export default function Shop(
       }
 ){
     const [currentPath, setCurrentPath] = useState<String>();
-    const windowPathname = usePathname();
     const changeLocation = (tab: string) => {
       setCurrentPath(() => tab)
      }
      useEffect(()=> {
+      const windowPathname = usePathname();
         setCurrentPath(windowPathname)
      }, [])
-     console.log(currentPath)
 
     return (
       <main className='h-fit min-h-screen px-4 md:px-8 w-screen md:overflow-x-hidden box-border pt-10 md:pt-20'>
